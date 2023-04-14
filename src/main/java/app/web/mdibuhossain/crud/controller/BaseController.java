e57f8ed2e98cf9738700867e9cce557ac6be9c5b
chadnipodder <chadnipodder@gmail.com>
2023-04-14 17:46:09 +0600

    updated

---
+++ src/main/java/app/web/mdibuhossain/crud/controller/BaseController.java
@@ -0,0 +1,9 @@
+package app.web.mdibuhossain.crud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public interface BaseController {
+}
--- src/main/java/app/web/mdibuhossain/crud/controller/BookController.java
+++ src/main/java/app/web/mdibuhossain/crud/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/book")
+@RequestMapping("api/v1/book")
 public class BookController {
     @Autowired
     private BookService bookService;
